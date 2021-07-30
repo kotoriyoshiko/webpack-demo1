349dbb04d2ee5acc423d4d53c0538df4a4984593
kotoriyoshiko <kortoriyoshiko@.outlook.com>
2021-07-30 14:26:19 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '方方',
+        template: 'src/assets/index.html'
     }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
