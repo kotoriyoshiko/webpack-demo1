5c4c64911fa0d178970f94cbe0d7d551ff709b2e
kotoriyoshiko <kortoriyoshiko@.outlook.com>
2021-07-30 15:57:37 +0800

    update2

---
+++ webpack.config.base.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    entry: './src/index.js',
+    output: {
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '方方',
+            template: 'src/assets/index.html'
+        }
+        ),
+    ]
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
+const base = require('./webpack.config.base.js')
+
 module.exports = {
-    mode: 'development',
+    ...base,
+    mode: 'production',
     devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    entry: './src/index.js',
-    output: {
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: '方方',
-        template: 'src/assets/index.html'
-    }
-    )],
+
     module: {
         rules: [
             {
